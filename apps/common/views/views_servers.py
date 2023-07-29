0b3e9dcdd9451e4466b5aecac06076a3054ff198
sevel <zhaobing.liu@outlook.com>
2023-07-29 23:07:27 +0800

    1.add(add server)

--- apps/common/views/views_servers.py
+++ apps/common/views/views_servers.py
@@ -1,11 +1,11 @@
 # -*- coding:utf-8 -*-
-from apps.common.models.models_files import FilesModel
+from apps.common.models.models_servers import ServersModel
 from apps.common.serializers.serializers_servers import ServersSerializer
 from utils.viewset import CustomModelViewSet
 
 
 class ServersView(CustomModelViewSet):
-    queryset = FilesModel.objects.all().order_by("-create_time")
+    queryset = ServersModel.objects.all().order_by("-create_time")
     serializer_class = ServersSerializer
     lookup_field = "id"
 
